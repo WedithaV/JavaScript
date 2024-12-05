cfef399ae1dd7898b45ea646ff8a4a2b662c84ef
WedithaV <wedithav@gmail.com>
2024-12-05 17:33:48 +0530

    Switch case

---
+++ if.js
@@ -0,0 +1,8 @@
+var x = 34;
+
+if(x > 0){
+    console.log("Positive");
+}
+else if(x < 0){
+    console.log("Negative");
+}
---
+++ switch-case.js
@@ -0,0 +1,15 @@
+var number = 2;
+
+switch(number){
+    case 1 :
+        console.log("One");
+        break;
+    case 2 :
+        console.log("Two");
+        break;
+    case 3 :
+        console.log("Three");
+        break;
+    default :
+        console.log("Number");
+}
