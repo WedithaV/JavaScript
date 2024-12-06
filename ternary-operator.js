c74869a4e0c5b2f17d76aabddd6c51ff9aed329d
WedithaV <wedithav@gmail.com>
2024-12-06 15:28:41 +0530

    Multi ternary operators

--- ternary-operator.js
+++ ternary-operator.js
@@ -1,2 +1,11 @@
 var x = (5 > 8) ? "5 is greater than 8" : "8 is greater than 5";
 console.log(x);
+
+
+
+//Multi ternary operators
+function checkSign(num){
+    return (num > 0) ? "Positive" : (num < 0) ? "Negative" : "Zero";
+}
+
+console.log(checkSign(0));
